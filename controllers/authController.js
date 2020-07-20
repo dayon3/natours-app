5d1df3fd6fa7a1841aaa4d93e3eb1c32f90fbf16
dayon3 <aliasdayone@gmail.com>
2020-07-20 13:08:34 +0300

    updating the current user password

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,18 +12,21 @@ const signToken = (id) => {
   });
 };
 
-exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
 
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
+  res.status(statusCode).json({
     status: 'success',
     token,
     data: {
-      user: newUser,
+      user,
     },
   });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -42,11 +45,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) if everything ok, send token to client
-  const token = signToken(user._id);
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -158,7 +157,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 2) if token has not expired, and there is user, set the new password
   if (!user) {
-    return next(new AppError('Token is invalid or hs expired', 400));
+    return next(new AppError('Token is invalid or has expired', 400));
   }
 
   user.password = req.body.password;
@@ -169,10 +168,24 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
   // 3) update changedPasswordAt property for the user
   // 4) log the user in, send JWT
-  const token = signToken(user._id);
+  createSendToken(user, 200, res);
+});
 
-  res.status(200).json({
-    status: 'success',
-    token,
-  });
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) get user from collection
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 1) check if POSTed password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 1) if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+  // User.findByIdAndUpdate will NOT work as intended!
+
+  // 1) log user in, send JWT
+  createSendToken(user, 200, res);
 });
