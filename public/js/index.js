9c49c8f86e90627fcbb1ff33930ad2a1cfb84d2e
dayon3 <aliasdayone@gmail.com>
2021-02-05 12:49:08 +0300

    Processing payments on the front-end

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { signup } from './signup';
 import { updateSettings } from './updateSettings';
+import { bookTour } from './stripe';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -12,6 +13,7 @@ const signupForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -69,3 +71,10 @@ if (userPasswordForm) {
     document.getElementById('password-confirm').value = '';
   });
 }
+
+if (bookBtn)
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+  });
---
+++ public/js/stripe.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(
+  'pk_test_51I3J0kFBZSA9mj3I1dlXxWaA6p74CbJMHaHSuYKYToMukUzsZH5mhP0J3N2tpR69Puqff1NwAXJAKMG4bQxGNe8K00T7OuJSQa'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
+
+    //  2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({ sessionId: session.data.session.id });
+  } catch (error) {
+    console.log(error);
+    showAlert('error', error);
+  }
+};
