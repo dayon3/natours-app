07e28ca4bd07e760760b7015468a01b620db0512
dayon3 <aliasdayone@gmail.com>
2021-02-15 21:11:38 +0300

    Added heroku config

--- server.js
+++ server.js
@@ -39,3 +39,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting dow gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
