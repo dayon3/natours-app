cdefcdc281cd44dbb980f7f6fec6f7a4a46bb413
dayon3 <aliasdayone@gmail.com>
2021-02-16 12:10:13 +0300

    📦 NEW: Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,18 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1: GLOBAL MIDDLEWARES
+// Implement CORS
+// pre-flight requests are already handled for all routes.
+app.use(cors());
+// app.options('*', cors());
+// Access-Control-Allow-origin *
+// api.natours.com, front-end natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
