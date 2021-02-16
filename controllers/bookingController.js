3ebf929bb965b0d640e882e01b539f4b023f72dd
dayon3 <aliasdayone@gmail.com>
2021-02-16 13:49:38 +0300

    🐛 FIX: stripe webhooks integration

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,7 +43,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
+  const tour = session?.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
@@ -64,7 +64,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.objectId);
+    createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
